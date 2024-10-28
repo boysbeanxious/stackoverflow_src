282e6a524686cb7979df479b4c5ca44a6bf7daa3
ComputationalSociologyLab <thecomputationalsociologylab@gmail.com>
2024-10-28 17:04:01 +0900

    20241028

--- shell/cog_com.sh
+++ shell/cog_com.sh
@@ -2,6 +2,9 @@
 
 echo "HelloWorld"
 
+tot_file_num=910265
+loop_num=1
+
 for file in ../../data/src/*.py
 do
 	dir_path=$(dirname $file)/
@@ -19,4 +22,13 @@ do
 	mv `echo $old_file` `echo $new_file`
 	cp `echo $new_file` `echo $dir_path`
 	\rm `echo $new_file`
+	
+	loop_num=$[$loop_num+1]
+	left_file_num=$[$tot_file_num-$loop_num]
+
+
+	echo "============================================="
+	echo "total file number : "$tot_file_num
+	echo "left file number : "$left_file_num
+	echo "============================================="
 done	
