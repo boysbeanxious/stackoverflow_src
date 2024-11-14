5e7bbbf9dc6d837c5438124b6f0e12c6ef0b6365
ComputationalSociologyLab <thecomputationalsociologylab@gmail.com>
2024-11-14 11:05:22 +0900

    20241113

--- lib/cc_cpp/CognitiveComplexityCalculator.py
+++ lib/cc_cpp/CognitiveComplexityCalculator.py
@@ -7,9 +7,17 @@ class CognitiveComplexityCalculator:
 
     def calculate(self, cpp_code):
         index = clang.cindex.Index.create()
-        tu = index.parse('temp.cpp', args=['-std=c++11'], unsaved_files=[('temp.cpp', cpp_code)])
-        self.visit_node(tu.cursor)
-        return self.complexity
+        try:
+            tu = index.parse('temp.cpp', args=['-std=c++11'], unsaved_files=[('temp.cpp', cpp_code)])
+            if len(tu.diagnostics) > 0:  # 구문 오류가 있는지 확인
+                for diag in tu.diagnostics:
+                    return (f"Syntax error: {diag.spelling}")  # 오류 메시지 출력
+                return None
+            self.visit_node(tu.cursor)
+            return self.complexity
+        except clang.cindex.TranslationUnitLoadError:
+            return ("Failed to parse C++ code.")
+            return None
 
     def visit_node(self, node, depth=0):
         # Cognitive Complexity Rule 1: Add 1 point for each decision point
---
+++ shell/cog_com_param_cpp.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "HelloWorld"
+
+tot_file_num=910265
+loop_num=1
+std_year=$1   
+src_path="../../data/src/"$std_year"_src"
+excel_path="../"$std_year"_excel/"
+
+echo $std_year
+cd $src_path
+
+
+tot_file_num=`ls -al | wc -l`
+
+for file in ./*.py
+do
+	dir_path=$(dirname $file)/
+	name=$(basename $file)
+	ex=".csv"
+	old_nm="complexipy"
+	new_nm="${name%.*}"
+
+	new_file="./$new_nm$ex"
+	old_file="./$old_nm$ex"
+	
+	#echo $dir_path
+	#echo $new_file
+	#echo $old_file
+	#echo $excel_path
+	
+	complexipy `echo $file` -l file -o
+	mv `echo $old_file` `echo $new_file`
+	cp `echo $new_file` `echo $excel_path`
+	\rm `echo $new_file`
+	
+	loop_num=$[$loop_num+1]
+	left_file_num=$[$tot_file_num-$loop_num]
+
+
+	echo "============================================="
+	echo "total file number : "$tot_file_num
+	echo "left file number : "$left_file_num
+	echo "============================================="
+done	
