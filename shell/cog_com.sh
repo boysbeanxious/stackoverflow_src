5248c61ff1f536249ab0554e522cca2caf15d521
ComputationalSociologyLab <thecomputationalsociologylab@gmail.com>
2025-05-07 20:34:39 +0900

    20250507

--- shell/cog_com.sh
+++ shell/cog_com.sh
@@ -2,10 +2,15 @@
 
 echo "HelloWorld"
 
-tot_file_num=910265
-loop_num=1
+src_path="../../data_bak/src/difficulty_src"
+excel_path="../difficulty_excel/"
 
-for file in ../../data/src/*.py
+
+cd $src_path
+tot_file_num=`ls -al | wc -l`
+
+echo $tot_file_num
+for file in ./*.py
 do
 	dir_path=$(dirname $file)/
 	name=$(basename $file)
@@ -15,12 +20,15 @@ do
 
 	new_file="./$new_nm$ex"
 	old_file="./$old_nm$ex"
-	echo $new_file
-	echo $old_file
+	
+	#echo $dir_path
+	#echo $new_file
+	#echo $old_file
+	#echo $excel_path
 	
 	complexipy `echo $file` -l file -o
 	mv `echo $old_file` `echo $new_file`
-	cp `echo $new_file` `echo $dir_path`
+	cp `echo $new_file` `echo $excel_path`
 	\rm `echo $new_file`
 	
 	loop_num=$[$loop_num+1]
