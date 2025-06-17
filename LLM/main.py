d5641e3a5a2138a86c73b5ff300bb03032a96781
ComputationalSociologyLab <thecomputationalsociologylab@gmail.com>
2025-06-17 10:11:16 +0900

    20250617

--- LLM/main.py
+++ LLM/main.py
@@ -13,29 +13,29 @@ def main(ver):
     print("start main! : ", ver)
     t_extract = qe.Q_Extract(ver)
 
-    while True :
-        print("t_extract start")
-        cnt = t_extract.chk_left()
-        print(f"t_extract end_{cnt}")
-        if cnt[0][0] > 0 : 
-            print(f"Q_Extract start_{cnt[0][1]}")
-            df = t_extract.db_extract()
-            q_output = t_extract.tb_extract(df)
-            print(f"Q_Extract end_{cnt[0][1]}")
+    # while True :
+    print("t_extract start")
+    cnt = t_extract.chk_left()
+    print(f"t_extract end_{cnt}")
+    if cnt[0][0] > 0 : 
+        print(f"Q_Extract start_{cnt[0][1]}")
+        df = t_extract.db_extract()
+        q_output = t_extract.tb_extract(df)
+        print(f"Q_Extract end_{cnt[0][1]}")
 
-            print(f"SampleSelf_Consistency start_{cnt[0][1]}")
-            sample_sc = ssc.SampleSelf_Consistency(q_output) 
-            print(f"SampleSelf_Consistency end_{cnt[0][1]}")
+        print(f"SampleSelf_Consistency start_{cnt[0][1]}")
+        sample_sc = ssc.SampleSelf_Consistency(q_output) 
+        print(f"SampleSelf_Consistency end_{cnt[0][1]}")
 
-            print(f"write_promt start_{cnt[0][1]}")
-            chk_list = sample_sc.write_promt()
-            print(f"write_promt end_{cnt[0][1]}")
+        print(f"write_promt start_{cnt[0][1]}")
+        chk_list = sample_sc.write_promt()
+        print(f"write_promt end_{cnt[0][1]}")
 
-            print(f"calc_acc_for_l start_{cnt[0][1]}")
-            result_df = sample_sc.calc_acc_for_l()
-            print(f"calc_acc_for_l end_{cnt[0][1]}")
-        else :
-            break
+        print(f"calc_acc_for_l start_{cnt[0][1]}")
+        result_df = sample_sc.calc_acc_for_l()
+        print(f"calc_acc_for_l end_{cnt[0][1]}")
+    else :
+        print("Nothing left")
 
 
 
