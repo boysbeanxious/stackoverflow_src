7e347f54bdf3ea51a8f2c877c4abd992fe7e5b7d
ComputationalSociologyLab <thecomputationalsociologylab@gmail.com>
2025-09-16 14:25:48 +0900

    20250916

--- lib/annotation/param.py
+++ lib/annotation/param.py
@@ -1,18 +1,20 @@
-param = {   'llm_model'     : 'l'
-         ,  'few_shot_n'    : 4
-         ,  'q_src_yn'      : 'Y'
-         ,  'p_ver'         :'sys_prompt8'
-         ,  'sf_num'        : 5
-         ,  'temperature'   : 0.01
-         ,  'excel_ver'     :'ver1'
-         }  
-
-
+# param for thesis
 # param = {   'llm_model'     : 'l'
 #          ,  'few_shot_n'    : 4
 #          ,  'q_src_yn'      : 'Y'
-#          ,  'p_ver'         :'sys_prompt9'
+#          ,  'p_ver'         :'sys_prompt8'
 #          ,  'sf_num'        : 5
 #          ,  'temperature'   : 0.01
 #          ,  'excel_ver'     :'ver1'
 #          }  
+
+
+# param for test
+param = {   'llm_model'     : 'l'
+         ,  'few_shot_n'    : 4
+         ,  'q_src_yn'      : 'Y'
+         ,  'p_ver'         :'sys_prompt10'
+         ,  'sf_num'        : 5
+         ,  'temperature'   : 0.01
+         ,  'excel_ver'     :'ver4'
+         }  
