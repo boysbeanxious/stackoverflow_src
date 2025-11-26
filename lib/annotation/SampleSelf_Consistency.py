1685cb0525fa8a5b124a9c7cccb419600b98309c
ComputationalSociologyLab <thecomputationalsociologylab@gmail.com>
2025-11-26 14:48:51 +0900

    20251126

--- lib/annotation/SampleSelf_Consistency.py
+++ lib/annotation/SampleSelf_Consistency.py
@@ -1,4 +1,5 @@
 from lib.annotation.import_files import *
+from lib.annotation.VLLM import VLLM
 # https://github.com/meta-llama/llama-recipes/blob/main/recipes/quickstart/Prompt_Engineering_with_Llama_3.ipynb
 class SampleSelf_Consistency:
     def __init__(self, annoate_target):  
@@ -29,7 +30,7 @@ class SampleSelf_Consistency:
 
         self.save_dir       = f'./result/{self.ver}'
 
-        print(f'param for sampel self consistency : {self.llm_model} | {self.few_shot_n} | {self.q_src_yn} | {self.sys_prompt} | {self.sf_num} | {self.temperature} | {self.excel_ver}' )
+        print(f'param for sample self consistency : {self.llm_model} | {self.few_shot_n} | {self.q_src_yn} | {self.sys_prompt} | {self.sf_num} | {self.temperature} | {self.excel_ver}' )
         if not os.path.exists(self.save_dir):
             os.makedirs(self.save_dir)  # 중첩된 폴더도 생성 가능
 
@@ -131,7 +132,18 @@ class SampleSelf_Consistency:
             cursor.close()
             conn.close()            
 
+    def calc_acc_for_v(self, llm_model, few_shot_n, q_src_yn):
+        vllm = VLLM()
+        for idx, message in tqdm(enumerate(self.message_list)):
+            tmp = []
+            response = vllm.llm.chat(message, sampling_params=vllm.params) 
 
+            tmp.append(self.df.loc[self.eval_q_list[idx], 'id'])
+            tmp.append(response[0].outputs[0].text)
+            self.result.append(tmp)
+        result_df = pd.DataFrame(self.result, columns = ['id', 'result'])
+        result_df = pd.merge(self.df, result_df, on = 'id')
+        result_df.to_csv(f'./result/sc_{llm_model}_result_{few_shot_n}_{self.test_n}_{q_src_yn}_{self.version}_{self.p_ver}_{self.sf_num}_{self.temperature}_{self.excel_ver}_{self.loop_i}.csv')
  
     def calc_acc_for_l(self):           
         for idx, message in tqdm(enumerate(self.message_list)):
@@ -183,3 +195,7 @@ class SampleSelf_Consistency:
         elif llm_model == 'c' : # chatgpt 
             # print(self.eval_prompt)
             self.calc_acc_for_c(llm_model, few_shot_n, q_src_yn)
+            
+        elif llm_model == 'v' : # vLLM
+            print("VLLM")
+            self.calc_acc_for_v(llm_model, few_shot_n, q_src_yn)
